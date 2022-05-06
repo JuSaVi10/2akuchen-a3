8e54f9d6ded1b5ed562404d752107029a77c3e42
luciasv10 <luciaserranovargas@gmail.com>
2022-05-06 10:07:48 +0200

    Cambio en index.php

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php session_start(); include_once "conexion.php";
-?>
-<form class="registro" action="" method="post">
-<div><label>Usuario:</label> <input type="text" name="usuario"></div>
-<div><label>Clave:</label> <input type="password" name="password"></div>
-<div><label>Repetir Clave:</label> <input type="password" name="repassword"></div>
-<div><input type="submit" name="enviar" value="Registrar"></div>
-</form>
-
+<?php session_start(); include_once "conexion.php";?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área privada</title>
+</head>
+<body>
+    <h1>Área privada</h1>
+    <form class="registro" action="" method="post">
+    <input type="text" placeholder="Usuario" name="usuario">
+    <input type="text" placeholder="Contraseña" name="password">
+    <input type="submit" value="Acceder" name="bbtn_enviar">
+    </form>
+</body>
+</html>
 
 
