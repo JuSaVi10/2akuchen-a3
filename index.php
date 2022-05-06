dbcf4707394fd166edf9e899e5a1d29426a53e6e
JuanSanchezVillarejo <74968141+JuanSanchezVillarejo@users.noreply.github.com>
2022-05-06 10:35:08 +0200

    cambio registro

--- index.php
+++ index.php
@@ -10,8 +10,11 @@
 <body>
     <h1>Área privada</h1>
     <form class="registro" action="" method="post">
-    <input type="text" placeholder="Usuario" name="usuario">
-    <input type="text" placeholder="Contraseña" name="password">
+    <input type="text" placeholder="Nombre" name="nombre">
+    <input type="text" placeholder="Nombre de Empresa" name="nombre_empresa">
+    <input type="text" placeholder="Dirección" name="direccion">
+    <input type="email" placeholder="Email" name="email">
+    <input type="password" placeholder="Contraseña" name="password">
     <input type="submit" value="Acceder" name="bbtn_enviar">
     </form>
 </body>
