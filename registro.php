eeeaeb6cd05066a1cd026222ea2899f21e829554
luciasv10 <luciaserranovargas@gmail.com>
2022-05-06 10:42:04 +0200

    Registro y Login
    He creado un archivo de registro y he cambiado los input de lado.

--- registro.php
+++ registro.php
@@ -9,12 +9,13 @@
 </head>
 <body>
     <h1>Área privada</h1>
-    <form class="login" action="" method="post">
+    <form class="registro" action="" method="post">
+    <input type="text" placeholder="Nombre" name="nombre">
+    <input type="text" placeholder="Nombre de Empresa" name="nombre_empresa">
+    <input type="text" placeholder="Dirección" name="direccion">
     <input type="email" placeholder="Email" name="email">
     <input type="password" placeholder="Contraseña" name="password">
     <input type="submit" value="Acceder" name="bbtn_enviar">
     </form>
 </body>
 </html>
-
-
