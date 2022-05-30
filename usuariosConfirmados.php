7663efa7ee396399d7b15095e4bba4135fea71ad
luciasv10 <luciaserranovargas@gmail.com>
2022-05-30 08:21:35 +0200

    Cambio nombre del archivo del estilo

--- usuariosConfirmados.php
+++ usuariosConfirmados.php
@@ -8,7 +8,7 @@ include "SED.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type ="text/css" href="css/j.css" screen = "all" />
+    <link rel="stylesheet" type ="text/css" href="css/style-confirm.css" screen = "all" />
     <title>Usuarios confirmados</title>
     
 </head>
